d665c95676c6c28c52ffec19c452949b777a7958
nick.butcher <butchern@mail.gvsu.edu>
2015-11-05 01:12:32 -0500

    Add NHibernate configuration and IoC setup, implement the generic repository and other supporting code.

---
+++ src/DataAccessTest/DataAccessTest.Repository.NHibernate/Mapping/ValueModelMapping.cs
@@ -0,0 +1,36 @@
+﻿namespace DataAccessTest.Repository.NHibernate.Mapping
+{
+    using DataAccessTest.Library.Value;
+    using global::NHibernate.Mapping.ByCode;
+    using global::NHibernate.Mapping.ByCode.Conformist;
+
+    /// <summary>
+    /// NHibernate database model mapping for the <see cref="ValueModel"/> class.
+    /// </summary>
+    public class ValueModelMapping : ClassMapping<ValueModel>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ValueModelMapping"/> class.
+        /// </summary>
+        public ValueModelMapping()
+        {
+            this.Table("Value");
+            this.Schema("dbo");
+            this.Id(
+                vm => vm.Id,
+                m =>
+                {
+                    m.Generator(Generators.Identity);
+                });
+
+            this.Property(
+                vm => vm.Description,
+                m =>
+                {
+                    m.Column("Desc");
+                    m.Length(256);
+                    m.NotNullable(true);
+                });
+        }
+    }
+}
--- src/DataAccessTest/DataAccessTest.Web/DependencyResolution/Registry/NHibernateRegistry.cs
+++ src/DataAccessTest/DataAccessTest.Web/DependencyResolution/Registry/NHibernateRegistry.cs
@@ -2,9 +2,14 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Web;
+    using DataAccessTest.Library.Value;
+    using DataAccessTest.Repository;
+    using DataAccessTest.Repository.NHibernate;
+    using NHibernate;
+    using NHibernate.Cfg;
+    using NHibernate.Mapping.ByCode;
     using StructureMap.Configuration.DSL;
+    using StructureMap.Pipeline;
 
     /// <summary>
     /// Configures dependency resolution for NHibernate.
@@ -16,7 +21,49 @@
         /// </summary>
         public NHibernateRegistry()
         {
-            // TODO: this space intentionally left blank
+            this.IncludeRegistry<SqlDatabaseRegistry>();
+            this.ForSingletonOf<Configuration>()
+                .Use<Configuration>()
+                .SelectConstructor(() => new Configuration());
+            this.ForSingletonOf<ISessionFactory>()
+                .Use(c => c.GetInstance<Configuration>().BuildSessionFactory());
+            this.For<ISession>()
+                .LifecycleIs<ContainerLifecycle>()
+                .Use(c => c.GetInstance<ISessionFactory>().OpenSession());
+            this.For<ITransaction>()
+                .LifecycleIs<ContainerLifecycle>()
+                .Use(c => c.GetInstance<ISession>().BeginTransaction());
+
+            this.Scan(s =>
+            {
+                s.AssemblyContainingType<NHibernateDbInitializer>();
+                s.AddAllTypesOf<IConformistHoldersProvider>();
+            });
+
+            For<IUnitOfWork>().LifecycleIs<ContainerLifecycle>().Use<NHibernateUnitOfWork>();
+
+            ForSingletonOf<IDataAccessInitializer>()
+                .Use<NHibernateDbInitializer>()
+                .Ctor<string>()
+                .Named("DefaultConnectionString")
+                .Ctor<Func<IEnumerable<IConformistHoldersProvider>>>()
+                .Is(c => c.GetAllInstances<IConformistHoldersProvider>);
+
+            // one line per entity
+            this.ConfigureGenericRepository<ValueModel>();
+        }
+
+        /// <summary>
+        /// Configure a generic NHibernate repository for type <typeparamref name="TModel"/>.
+        /// </summary>
+        /// <typeparam name="TModel">The type of the repository model.</typeparam>
+        private void ConfigureGenericRepository<TModel>()
+            where TModel : class
+        {
+            this.For<NHibernateGenericRepository<TModel>>().LifecycleIs<ContainerLifecycle>();
+            this.Forward<NHibernateGenericRepository<TModel>, IGenericReadRepository<TModel>>();
+            this.Forward<NHibernateGenericRepository<TModel>, IGenericWriteRepository<TModel>>();
+            this.Forward<NHibernateGenericRepository<TModel>, IGenericRepository<TModel>>();
         }
     }
 }
